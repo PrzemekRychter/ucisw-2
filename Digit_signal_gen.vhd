2d9e17e8b2a67e107866ebc7205f89df515d3216
Przemek Rychter <przemek.rych89@gmail.com>
2021-04-28 15:04:44 +0200

    init

---
+++ Digit_signal_gen.vhd
@@ -0,0 +1,95 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    17:29:06 04/10/2021 
+-- Design Name: 
+-- Module Name:    Dec_sign_generator - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all; 
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Dec_sign_generator is
+    Port ( Rdy : in  STD_LOGIC;
+           Clk : in  STD_LOGIC;
+			  Key : in STD_LOGIC_VECTOR (3 downto 0);
+           Command : out  STD_LOGIC_VECTOR (3 downto 0);
+           Address : out  STD_LOGIC_VECTOR (3 downto 0);
+           Data : out  STD_LOGIC_VECTOR (11 downto 0);
+           Start : out  STD_LOGIC);
+end Dec_sign_generator;
+
+architecture Behavioral of Dec_sign_generator is
+
+	signal clks_delay : integer := 999;
+	signal clk_counter : integer := 0;
+	signal var : unsigned (11 downto 0) := X"000";
+begin
+
+	with Key select clks_delay <=
+		1275 when "0000",	-- 784  g2		[ HZ ]
+		1203 when "0001", -- 830,6 gis2
+		1135 when "0010", -- 880,0 a2
+		1072 when "0011", -- 932,3 b2
+		1011 when "0100", -- 987,8 h2
+		955 when "0101", 	-- 1046,5 c3
+		901 when "0110", 	-- 1108,7 cis3
+		850 when "0111", 	-- 1174,7 d3
+		803 when "1000",  -- 1244,5 dis3
+		757 when "1001",	-- 1318,5 e3
+		715 when "1010",	-- 1396,9 f3
+		675 when "1011",	-- 1480,0 fis3
+		0 	 when others;
+	
+	Command <= "0011";
+	Address <= "0000";
+	
+	process(Clk)
+	begin
+			
+		if rising_edge(Clk) and Rdy = '1' then
+			if clks_delay /= 0 then --
+				if clk_counter < clks_delay then
+					clk_counter <= clk_counter + 1;
+					Start <= '0';
+				else
+					Start <= '1';
+					if var < X"FAD" then
+						var <=  var + 82;
+					else 
+						var <= X"000";
+					end if;
+					clk_counter <= 0;
+				end if;
+			else
+				var <= X"000";
+				clk_counter <= 0;
+			end if;
+		end if;
+		
+	end process;
+	
+	Data <= STD_LOGIC_VECTOR(var);
+	
+end Behavioral;
---
+++ Digit_signal_gen_tb.vhd
@@ -0,0 +1,103 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   23:10:55 04/10/2021
+-- Design Name:   
+-- Module Name:   C:/Users/artur/OneDrive/Pulpit/UCiSW_2/Projekt/Digit_signal_gen_tb.vhd
+-- Project Name:  Projekt
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: Dec_sign_generator
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY Digit_signal_gen_tb IS
+END Digit_signal_gen_tb;
+ 
+ARCHITECTURE behavior OF Digit_signal_gen_tb IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT Dec_sign_generator
+    PORT(
+         Rdy : IN  std_logic;
+         Clk : IN  std_logic;
+			Key : in STD_LOGIC_VECTOR (3 downto 0);
+         Command : OUT  std_logic_vector(3 downto 0);
+         Address : OUT  std_logic_vector(3 downto 0);
+         Data : OUT  std_logic_vector(11 downto 0);
+         Start : OUT  std_logic
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal Rdy : std_logic := '0';
+   signal Clk : std_logic := '0';
+	signal Key : STD_LOGIC_VECTOR (3 downto 0) := "0000";
+ 	--Outputs
+   signal Command : std_logic_vector(3 downto 0);
+   signal Address : std_logic_vector(3 downto 0);
+   signal Data : std_logic_vector(11 downto 0);
+   signal Start : std_logic;
+
+   -- Clock period definitions
+   constant Clk_period : time := 20 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: Dec_sign_generator PORT MAP (
+          Rdy => Rdy,
+          Clk => Clk,
+			 Key => Key,
+          Command => Command,
+          Address => Address,
+          Data => Data,
+          Start => Start
+        );
+
+   -- Clock process definitions
+   Clk_process :process
+   begin
+		Clk <= '0';
+		wait for Clk_period/2;
+		Clk <= '1';
+		wait for Clk_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+      Key <= "0000", "1100" after 3 ms, "0011" after 6 ms;
+		wait for 100 ns;	
+      Rdy <= '1';
+		
+		
+      wait;
+   end process;
+
+END;

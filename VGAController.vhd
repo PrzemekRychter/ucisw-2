fc9f35930dc5a1798ac720411f62cfd333db2633
arturex178 <49100889+arturex178@users.noreply.github.com>
2021-05-27 10:01:17 +0200

    Updated testbench and VGAController

--- VGAController.vhd
+++ VGAController.vhd
@@ -39,8 +39,11 @@ ENTITY VGAController IS
 END VGAController;
 
 ARCHITECTURE Behavioral OF VGAController IS
-	TYPE state_type IS (Silence, PrintKey, WaitForOther, Space, SecondsS, Dot, FractionsOfSecond, Space2);
-	SIGNAL state, next_state : state_type;
+	TYPE state_type IS (Silence, PrintKey, WaitForOther, Space, SecondsS, Dot, FractionsOfSecond, 
+								Space2, SecondsSilence, SpaceSilence, DotSilence, FractionsOfSecondSilence,
+								Space2Silence, DrawSilence, Space3);
+	SIGNAL state : state_type := DrawSilence;
+	SIGNAL next_state : state_type;
 	SIGNAL lastKey : STD_LOGIC_VECTOR(3 DOWNTO 0);
 	SIGNAL counter, tensOfASecond, seconds : integer := 0;
 	SIGNAL tensOfASecondToDisplay, secondsToDisplay : STD_LOGIC_VECTOR(3 DOWNTO 0);
@@ -51,39 +54,65 @@ BEGIN
 	BEGIN
 		IF rising_edge(Clk) THEN
 				state <= next_state;
-				lastKey <= key;
+		END IF;
+	END PROCESS;
+	
+	checkLastKeyProcess : PROCESS (Clk)
+	BEGIN
+		IF rising_edge(Clk) THEN
+				lastKey <= key; -- oddzielny proces
 		END IF;
 	END PROCESS;
 
-	logicProcess : PROCESS (state, key)
+	logicProcess : PROCESS (state, key, lastKey)
 		BEGIN
 		next_state <= state; --default
 		CASE state IS
-			WHEN Silence 			  => 
-					IF key /= "1111"	THEN next_state <= PrintKey;
+			WHEN Silence 			  => next_state <= Silence; 
+					IF key /= "1111"	THEN next_state <= SecondsSilence;			-- PrintKey;
 					ELSE next_state <= Silence; 
 					END IF;
-			WHEN PrintKey 		     => next_state <= WaitForOther;
-			WHEN WaitForOther      =>
-					IF key /= lastKey THEN
-						next_state <= Space;
-					END IF;
-			WHEN Space 			     => next_state <= SecondsS;
-			WHEN SecondsS 			  => next_state <= Dot;
-			WHEN Dot					  => next_state <= FractionsOfSecond;
-			WHEN FractionsOfSecond => next_state <= Space2;
-			WHEN Space2 			  => next_state <= Silence;
-			WHEN OTHERS 			  => next_state <= Silence;
+
+			WHEN SecondsSilence  			=> next_state <= DotSilence;
+			WHEN DotSilence	   			=> next_state <= FractionsOfSecondSilence;
+			WHEN FractionsOfSecondSilence => next_state <= Space2Silence;
+			WHEN Space2Silence				=> next_state <= PrintKey;
+			WHEN PrintKey 		     			=> next_state <= WaitForOther;
+			WHEN WaitForOther      			=>
+				IF key /= lastKey THEN
+					next_state <= Space;
+				END IF;
+			WHEN Space 			     			=> next_state <= SecondsS;
+			WHEN SecondsS 			  			=> next_state <= Dot;
+			WHEN Dot					  			=> next_state <= FractionsOfSecond;
+			WHEN FractionsOfSecond 			=> next_state <= Space2;
+			WHEN Space2 			  			=> next_state <= DrawSilence;
+			WHEN DrawSilence		  			=> next_state <= Space3;
+			WHEN Space3				  			=> next_state <= Silence;
+			WHEN OTHERS 			  			=> next_state <= Silence;
 		END CASE;
 	END PROCESS;
 
 
 					
-	outputProcess : PROCESS (state)
+	outputProcess : PROCESS (state, secondsToDisplay, tensOfASecondToDisplay, key)
 	BEGIN
 		CASE state IS
-			WHEN Silence => 
+			WHEN Silence =>
+				char_DI <= "00000000"; -- Avoiding latch
 				char_WE <= '0';
+			WHEN SecondsSilence =>
+				char_DI <= std_logic_vector(X"3" & secondsToDisplay(3 DOWNTO 0));
+				char_WE <= '1';
+			WHEN DotSilence =>
+				char_DI <= "00101110"; -- Dot	
+				char_WE <= '1';
+			WHEN FractionsOfSecondSilence =>
+				char_DI <= std_logic_vector(X"3" & tensOfASecondToDisplay(3 DOWNTO 0));
+				char_WE <= '1';
+			WHEN Space2Silence => 
+				char_DI <= "00100000"; -- Space
+				char_WE <= '1';
 			WHEN PrintKey =>		
 																		-- znak na ekranie   Nuta
 				IF 	key = "0000" THEN char_DI <= x"63"; -- c      			   - C
@@ -98,10 +127,12 @@ BEGIN
 				ELSIF key = "1001" THEN char_DI <= X"61"; -- a 						- A
 				ELSIF key = "1010" THEN char_DI <= X"62"; -- b 						- B
 				ELSIF key = "1011" THEN char_DI <= X"68"; -- h 						- H
-				ELSE  char_DI <= X"00"; 
+				ELSIF key = "1111" THEN char_DI <= X"23"; -- # 						- CISZA
+				ELSE  char_DI <= X"00";
 				END IF;
 				char_WE <= '1';
 			WHEN WaitForOther => 
+				char_DI <= "00000000"; -- Avoiding latch
 				char_WE <= '0';
 			WHEN Space             => 
 				char_WE <= '1';
@@ -118,17 +149,26 @@ BEGIN
 			WHEN Space2				  => 
 				char_DI <= "00100000"; -- Space
 				char_WE <= '1';
+			WHEN Space3				  => 
+				char_DI <= "00100000"; -- Space
+				char_WE <= '1';
+			WHEN DrawSilence		  =>
+				char_DI <= X"23";
+				char_WE <= '1';
 			WHEN OTHERS => 
+				char_DI <= "00000000"; -- Avoiding latch
 				char_WE <= '0';
 		END CASE;
 	END PROCESS;
 
 	--CLK freq = 50 Mhz T = 1/(50*10^6) s
 	-- 1/10s = T * 5 * 10^6
-	countToTenthOfASecond : PROCESS (Clk)
+	
+	
+	countToTenthOfAMiliSecond : PROCESS (Clk)
 	BEGIN
 		IF (rising_edge(Clk)) THEN
-			IF counter = 5000000 THEN
+			IF counter = 5000 THEN -- 5000000 <- 0,1 s
 				counter <= 0;
 			ELSE
 				counter <= counter + 1;
@@ -140,10 +180,10 @@ BEGIN
 		END IF;
 	END PROCESS ;
 	
-	countTensOfASecond : PROCESS (Clk)
+	countTensOfAMiliSecond : PROCESS (Clk)
 	BEGIN
 		IF (rising_edge(Clk)) THEN
-			IF counter = 5000000 THEN -- jeśli policzyłes 1/10 s
+			IF counter = 5000 THEN -- jeśli policzyłes 1/10 s
 				IF tensOfASecond = 9 THEN
 					tensOfASecond <= 0;
 				ELSE
@@ -158,7 +198,7 @@ BEGIN
 		END IF;
 	END PROCESS;
 
-	countSecondsS : PROCESS (Clk)
+	countMiliSecondsS : PROCESS (Clk)
 	BEGIN
 		IF (rising_edge(Clk)) THEN
 			IF tensOfASecond = 9 THEN
--- raw_main_tb.vhd
+++ raw_main_tb.vhd
@@ -1,4 +1,4 @@
--- Vhdl test bench created from schematic /home/przem/Documents/Semestr6/UCiSW 2/Projekt/Organy/raw_main.sch - Thu Apr 29 10:29:58 2021
+-- Vhdl test bench created from schematic C:\Users\artur\OneDrive\Pulpit\UCiSW2\organki\raw_main.sch - Wed May 26 11:56:20 2021
 --
 -- Notes: 
 -- 1) This testbench template has been automatically generated using types
@@ -23,54 +23,102 @@ ARCHITECTURE behavioral OF raw_main_raw_main_sch_tb IS
 
    COMPONENT raw_main
    PORT( CLK	:	IN	STD_LOGIC; 
-          DO_Rdy	:	IN	STD_LOGIC; 
-          F0	:	IN	STD_LOGIC; 
           DO	:	IN	STD_LOGIC_VECTOR (7 DOWNTO 0); 
           Start	:	OUT	STD_LOGIC; 
           Command	:	OUT	STD_LOGIC_VECTOR (3 DOWNTO 0); 
           Address	:	OUT	STD_LOGIC_VECTOR (3 DOWNTO 0); 
-          Data	:	OUT	STD_LOGIC_VECTOR (11 DOWNTO 0));
+          Data	:	OUT	STD_LOGIC_VECTOR (11 DOWNTO 0); 
+          DO_Rdy	:	IN	STD_LOGIC; 
+          F0	:	IN	STD_LOGIC; 
+          Busy	:	OUT	STD_LOGIC; 
+          VGA_RGB	:	OUT	STD_LOGIC; 
+          VGA_VS	:	OUT	STD_LOGIC; 
+          VGA_HS	:	OUT	STD_LOGIC);
    END COMPONENT;
 
    SIGNAL CLK	:	STD_LOGIC;
-   SIGNAL DO_Rdy	:	STD_LOGIC;
-   SIGNAL F0	:	STD_LOGIC;
    SIGNAL DO	:	STD_LOGIC_VECTOR (7 DOWNTO 0);
    SIGNAL Start	:	STD_LOGIC;
    SIGNAL Command	:	STD_LOGIC_VECTOR (3 DOWNTO 0);
    SIGNAL Address	:	STD_LOGIC_VECTOR (3 DOWNTO 0);
    SIGNAL Data	:	STD_LOGIC_VECTOR (11 DOWNTO 0);
+   SIGNAL DO_Rdy	:	STD_LOGIC;
+   SIGNAL F0	:	STD_LOGIC;
+   SIGNAL Busy	:	STD_LOGIC;
+   SIGNAL VGA_RGB	:	STD_LOGIC;
+   SIGNAL VGA_VS	:	STD_LOGIC;
+   SIGNAL VGA_HS	:	STD_LOGIC;
 
-   -- Clock period definitions
+		 -- Interface to VGAscaner
+   component VGAscan
+       Generic ( -- VGA mode params (defaults are for 800x600/72Hz)
+                 pxWidth    : integer := 800;         -- visible pixels: width
+                 pxHeight   : integer := 600;         -- visible pixels: height
+                 pxHSfrontP : integer := 56;          -- HSync front porch
+                 lnVSfrontP : integer := 37;          -- VSync front porch
+                 pxTotal    : integer := 1040;        -- total horiz. pixels (visible & invisible)
+                 lnTotal    : integer := 666;         -- total vert. lines (visible & invisible)
+                 mhzPixelClk: real    := 50.000;      -- pixel clock
+                 activeHS   : std_logic := '1';       -- HSync polarity
+                 activeVS   : std_logic := '1';       -- VSync polarity
+                 -- Aux params
+                 pxBorder   : integer := 2;           -- bitmap border around the visible area
+                 FileName   : string := "C:\Users\artur\OneDrive\Pulpit\UCiSW2\organki\frame\frame"    -- output file name (frame number & ".bmp" will be appended)
+               );
+       Port ( VS, HS : in  STD_LOGIC;
+              R, G, B : in  STD_LOGIC);
+   end component;
+	
+	   -- Clock period definitions
    constant Clk_period : time := 20 ns;	-- 50 Mhz
 	
 BEGIN
 
    UUT: raw_main PORT MAP(
 		CLK => CLK, 
-		DO_Rdy => DO_Rdy, 
-		F0 => F0, 
 		DO => DO, 
 		Start => Start, 
 		Command => Command, 
 		Address => Address, 
-		Data => Data
+		Data => Data, 
+		DO_Rdy => DO_Rdy, 
+		F0 => F0, 
+		Busy => Busy, 
+		VGA_RGB => VGA_RGB, 
+		VGA_VS => VGA_VS, 
+		VGA_HS => VGA_HS
    );
-	
+
+	 ------------------------------------------------------------------
+   -- Instance of the VGA scanner
+   ------------------------------------------------------------------
+   I_VGAscaner: VGAscan
+      generic map(
+         pxBorder => 3,
+         FIleName => "C:\Users\artur\OneDrive\Pulpit\UCiSW2\organki\frame\frame" )    -- VGAtxt48x20 works in 800x600/72Hz mode => VGA generics can be left with defaults
+      port map(
+         VS => VGA_VS,
+         HS => VGA_HS,
+         R => VGA_RGB,
+         G => VGA_RGB,
+         B => VGA_RGB );
+
+
+
    -- Clock process definitions
    Clk_process :process
    begin
-		Clk <= '0';
+		CLK <= '0';
 		wait for Clk_period/2;
-		Clk <= '1';
+		CLK <= '1';
 		wait for Clk_period/2;
    end process;
 	
    -- Stimulus process
    tb: process
 		type typeByteArray is array ( NATURAL range <> ) of STD_LOGIC_VECTOR( 7 downto 0 );
-		variable arrBytes : typeByteArray( 0 to 12 ):= ( X"1C", X"1B", X"23", X"2B", X"34", X"33", X"3B", X"1D", X"24", X"2C", X"34", X"3C", X"3A");
-	begin		
+		variable arrBytes : typeByteArray( 0 to 12 ):= ( X"1C", X"1B", X"23", X"2B", X"34", X"33", X"3B", X"1D", X"24", X"2C", X"35", X"3C", X"3A");
+	begin
 		for i in arrBytes'RANGE loop
 			DO <= arrBytes(i);
 			DO_Rdy <= '1';
@@ -82,8 +130,33 @@ BEGIN
 			F0 <= '1';
 			wait for Clk_period;
 			DO_Rdy <= '0';
-			wait for 1 ms - Clk_period;
+			wait for 1.5 ms - Clk_period;
 		end loop;
+		
+			DO <= arrBytes(0);
+			DO_Rdy <= '1';
+			F0 <= '0';
+			wait for Clk_period;
+			DO_Rdy <= '0';
+			wait for 3 ms - Clk_period;
+			DO_Rdy <= '1';
+			F0 <= '1';
+			wait for Clk_period;
+			DO_Rdy <= '0';	
+			wait for Clk_period;
+			DO <= arrBytes(1);
+			DO_Rdy <= '1';
+			F0 <= '0';
+			wait for Clk_period;
+			DO_Rdy <= '0';
+			wait for 4 ms - Clk_period;
+			DO_Rdy <= '1';
+			F0 <= '1';
+			wait for Clk_period;
+			DO_Rdy <= '0';
+
+			-- wait for 1.5 ms - Clk_period;
+		
 	wait;
    end process;
 
